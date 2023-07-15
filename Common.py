29d3f428fbdd43c8ab7fbb7402581299daa96c13
ambroisemuller <ambroisemuller3@gmail.com>
2023-07-15 18:36:04 +0200

    cleanup

--- Common.py
+++ Common.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 import torch
 import os
 import time
@@ -101,55 +102,236 @@ def fit(model, training_set, num_epochs, optimizer, p, verbose=True):
     return history, mse, run_time / num_epochs
     
 
-class Legendre(nn.Module):
-    """ Univariate Legendre Polynomial """
+import torch.nn as nn
+import torch
+import os
+import time
 
-    def __init__(self, PolyDegree):
-        super(Legendre, self).__init__()
-        self.degree = PolyDegree
-
-    def legendre(self,x, degree):
-        x = x.reshape(-1, 1)
-        list_poly = list()
-        zeroth_pol = torch.ones(x.size(0),1)
-        list_poly.append(zeroth_pol)
-        # retvar[:, 0] = x * 0 + 1
-        if degree > 0:
-            first_pol = x
-            list_poly.append(first_pol)
-            ith_pol = torch.clone(first_pol)
-            ith_m_pol = torch.clone(zeroth_pol)
-
-            for ii in range(1, degree):
-                ith_p_pol = ((2 * ii + 1) * x * ith_pol - ii * ith_m_pol) / (ii + 1)
-                list_poly.append(ith_p_pol)
-                ith_m_pol = torch.clone(ith_pol)
-                ith_pol = torch.clone(ith_p_pol)
-        list_poly = torch.cat(list_poly,1)
-        return list_poly
-
-    def forward(self, x):
-        eval_poly = self.legendre(x, self.degree)
-        return eval_poly
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+torch.manual_seed(42)
 
 
+class NeuralNet(nn.Module):
 
+    def __init__(self, input_dimension, output_dimension, n_hidden_layers, neurons, regularization_param, regularization_exp, retrain_seed):
+        super(NeuralNet, self).__init__()
+        # Number of input dimensions n
+        self.input_dimension = input_dimension
+        # Number of output dimensions m
+        self.output_dimension = output_dimension
+        # Number of neurons per layer
+        self.neurons = neurons
+        # Number of hidden layers
+        self.n_hidden_layers = n_hidden_layers
+        # Activation function
+        self.activation = nn.Tanh()
+        self.regularization_param = regularization_param
+        # Regularization exponent
+        self.regularization_exp = regularization_exp
+        # Random seed for weight initialization
 
-class MultiVariatePoly(nn.Module):
-
-    def __init__(self, dim, order):
-        super(MultiVariatePoly, self).__init__()
-        self.order = order
-        self.dim = dim
-        self.polys = Legendre(order)
-        self.num = (order + 1) ** dim
-        self.linear = torch.nn.Linear(self.num, 1)
+        self.input_layer = nn.Linear(self.input_dimension, self.neurons)
+        self.hidden_layers = nn.ModuleList([nn.Linear(self.neurons, self.neurons) for _ in range(n_hidden_layers - 1)])
+        self.output_layer = nn.Linear(self.neurons, self.output_dimension)
+        self.retrain_seed = retrain_seed
+        # Random Seed for weight initialization
+        self.init_xavier()
 
     def forward(self, x):
-        poly_eval = list()
-        leg_eval = torch.cat([self.polys(x[:, i]).reshape(1, x.shape[0], self.order + 1) for i in range(self.dim) ])
-        for i in range(x.shape[0]):
-            poly_eval.append(torch.torch.cartesian_prod(*leg_eval[:, i, :]).prod(dim=1).view(1, -1))
-        poly_eval = torch.cat(poly_eval)
-        return self.linear(poly_eval)
+        # The forward function performs the set of affine and non-linear transformations defining the network
+        # (see equation above)
+        x = self.activation(self.input_layer(x))
+        for k, l in enumerate(self.hidden_layers):
+            x = self.activation(l(x))
+        return self.output_layer(x)
 
+    def init_xavier(self):
+        torch.manual_seed(self.retrain_seed)
+
+        def init_weights(m):
+            if type(m) == nn.Linear and m.weight.requires_grad and m.bias.requires_grad:
+                g = nn.init.calculate_gain('tanh')
+                torch.nn.init.xavier_uniform_(m.weight, gain=g)
+                # torch.nn.init.xavier_normal_(m.weight, gain=g)
+                m.bias.data.fill_(0)
+
+        self.apply(init_weights)
+
+    def regularization(self):
+        reg_loss = 0
+        for name, param in self.named_parameters():
+            if 'weight' in name:
+                reg_loss = reg_loss + torch.norm(param, self.regularization_exp)
+        return self.regularization_param * reg_loss
+
+
+def fit(model, training_set, num_epochs, optimizer, p, test_set=None, verbose=True):
+    history = []
+    test_loss = []
+    start_time = time.time()  # Start measuring time
+
+    # Loop over epochs
+    for epoch in range(num_epochs):
+        if verbose: print("################################ ", epoch, " ################################")
+
+        running_loss = list([0])
+
+
+        # Loop over batches
+        for j, (x_train_, u_train_) in enumerate(training_set):
+            def closure():
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                # forward + backward + optimize
+                u_pred_ = model(x_train_)
+                # Item 1. below
+                loss = torch.mean((u_pred_.reshape(-1, ) - u_train_.reshape(-1, )) ** p) + model.regularization()
+                # Item 2. below
+                loss.backward()
+                # Compute average training loss over batches for the current epoch
+                running_loss[0] += loss.item()
+                return loss
+
+            # Item 3. below
+            optimizer.step(closure=closure)
+
+        running_test_loss = 0
+        if test_set != None:
+            for j, (x_test_, u_test_) in enumerate(test_set):
+                u_pred_test_ = model(x_test_)
+                loss = torch.mean((u_pred_test_.reshape(-1, ) - u_test_.reshape(-1, )) ** p)
+                running_test_loss += loss.item()/len(test_set)
+
+        if verbose: print('Training loss: ', (running_loss[0] / len(training_set)), "Test loss: ", (running_test_loss))
+        history.append(running_loss[0] / len(training_set))
+        test_loss.append(running_test_loss)
+
+    end_time = time.time()  # Stop measuring time
+    run_time = end_time - start_time
+
+    print("Train loss:", history)
+    print("Test loss:", test_loss)
+
+    return history, run_time / num_epochs
+    
+
+def activation(name):
+    if name in ['tanh', 'Tanh']:
+        return nn.Tanh()
+    elif name in ['relu', 'ReLU']:
+        return nn.ReLU(inplace=True)
+    elif name in ['lrelu', 'LReLU']:
+        return nn.LeakyReLU(inplace=True)
+    elif name in ['sigmoid', 'Sigmoid']:
+        return nn.Sigmoid()
+    elif name in ['softplus', 'Softplus']:
+        return nn.Softplus(beta=4)
+    elif name in ['celu', 'CeLU']:
+        return nn.CELU()
+    elif name in ['elu']:
+        return nn.ELU()
+    elif name in ['mish']:
+        return nn.Mish()
+    else:
+        raise ValueError('Unknown activation function')
+
+##### 2D FOURIER LAYER #####
+
+class SpectralConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1, modes2):
+        super(SpectralConv2d, self).__init__()
+
+        """
+        2D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
+        """
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1  # Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes2 = modes2
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+
+    # Complex multiplication
+    def compl_mul2d(self, input, weights):
+        # (batch, in_channel, x,y ), (in_channel, out_channel, x,y) -> (batch, out_channel, x,y)
+        return torch.einsum("bixy,ioxy->boxy", input, weights)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        # Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfft2(x)
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels, x.size(-2), x.size(-1) // 2 + 1, dtype=torch.cfloat, device=x.device)
+        out_ft[:, :, :self.modes1, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
+
+        # Return to physical space
+        x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
+        return x
+
+
+class FNO2d(nn.Module):
+    def __init__(self, fno_architecture, device=None, padding_frac=1 / 4):
+        super(FNO2d, self).__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+
+        input: the solution of the coefficient function and locations (a(x, y), x, y)
+        input shape: (batchsize, x=s, y=s, c=3)
+        output: the solution 
+        output shape: (batchsize, x=s, y=s, c=1)
+        """
+        self.modes1 = fno_architecture["modes"]
+        self.modes2 = fno_architecture["modes"]
+        self.width = fno_architecture["width"]
+        self.n_layers = fno_architecture["n_layers"]
+        self.retrain_fno = fno_architecture["retrain_fno"]
+
+        torch.manual_seed(self.retrain_fno)
+        # self.padding = 9 # pad the domain if input is non-periodic
+        self.padding_frac = padding_frac
+        self.fc0 = nn.Linear(3, self.width)  # input channel is 3: (a(x, y), x, y)
+        
+        self.conv_list = nn.ModuleList(
+            [nn.Conv2d(self.width, self.width, 1) for _ in range(self.n_layers)])
+        self.spectral_list = nn.ModuleList(
+            [SpectralConv2d(self.width, self.width, self.modes1, self.modes2) for _ in range(self.n_layers)])
+
+        self.fc1 = nn.Linear(self.width, 128)
+        self.fc2 = nn.Linear(128, 1)
+
+        self.to(device)
+
+    def forward(self, x):
+        x = self.fc0(x)
+        x = x.permute(0, 3, 1, 2)
+
+        x1_padding = int(round(x.shape[-1] * self.padding_frac))
+        x2_padding = int(round(x.shape[-2] * self.padding_frac))
+        x = F.pad(x, [0, x1_padding, 0, x2_padding])
+
+        for k, (s, c) in enumerate(zip(self.spectral_list, self.conv_list)):
+
+            x1 = s(x)
+            x2 = c(x)
+            x = x1 + x2
+            if k != self.n_layers - 1:
+                x = F.gelu(x)
+        x = x[..., :-x1_padding, :-x2_padding]
+
+        x = x.permute(0, 2, 3, 1)
+        x = self.fc1(x)
+        x = F.gelu(x)
+        x = self.fc2(x)
+        return x
